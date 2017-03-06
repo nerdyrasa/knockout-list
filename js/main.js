e9fb01c20b77b6237dd4001bdedee2f4390abe8d
RB Isaacson <nerdyrasa@users.noreply.github.com>
2017-03-06 11:40:03 -0600

    Clean up.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@
  * Created by rasai_000 on 3/2/2017.
  */
 
+
+
 app.placeViewModel.init();
 ko.applyBindings(app.placeViewModel);
--- js/viewmodels/placeViewModel.js
+++ js/viewmodels/placeViewModel.js
@@ -5,42 +5,25 @@ var app = window.app || {};
 app.placeViewModel = (function (ko, db) {
   "use strict";
 
-
   var me = {
     places: ko.observableArray([]),   // main array
 
-    query: ko.observable(""),
-
-    filteredPlaces: undefined,
     selectedCategory: ko.observable(),
     selectedName: ko.observable(''),
     categories: undefined,
 
     numberOfClicks: ko.observable(0),
-    incrementClickCounter: incrementClickCounter,
+    processClickOnListItem: processClickOnListItem,
     init: init
   };
 
-  function filteredPlaces() {
-    ko.computed(function () {
-      var filter = me.query().toLowerCase();
 
-      if (!filter) {
-        return me.places();
-      } else {
-        return ko.utils.arrayFilter(me.places(), function (item) {
-          return item.name().toLowerCase().indexOf(filter) !== -1;
-        });
-      }
-    });
-  }
 
 
   me.categories = ko.computed(function(){
     var results = this.places(),
         filterByCategory = this.selectedCategory();
 
-    console.log("filterByCategory = ", filterByCategory);
     if (filterByCategory) {
       results = ko.utils.arrayFilter(results, function(category){
         console.log("match " + category.type + ", " + filterByCategory);
@@ -52,6 +35,9 @@ app.placeViewModel = (function (ko, db) {
     return results;
   }, me);
 
+
+  // A list of unique categories generated from the category.type of each place
+  // used to populate the select options
   me.categoryName = ko.computed(function(){
     var results = ko.utils.arrayMap(this.places(), function(category){
       return category.type;
@@ -59,35 +45,33 @@ app.placeViewModel = (function (ko, db) {
     return results.filter( onlyUnique );
   }, me);
 
+  // helper function
   // http://stackoverflow.com/questions/1960473/unique-values-in-an-array
-
   function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
   }
 
+
+  // loading data from the json file here
   function init() {
     db.getPlaces(function (data) {
-      console.log("getting place data");
-      console.log("data = ", data);
       var a = [];
       ko.utils.arrayForEach(data || [], function (item) {
-        console.log("item = ", item);
+ //       console.log("item = ", item);
         a.push(new app.Place(item.type, item.name, item.location));
       });
       me.places(a);
-      console.log("a = ", a);
       console.log("places = ", me.places()); // remember to unwrap observable using parentheses
     });
   }
 
-  function incrementClickCounter(place) {
-    //console.log("place = ", place);
-    console.log("place name = ", place.name);
-    var previousCount = me.numberOfClicks();
-    me.numberOfClicks(previousCount + 1);
-    console.log("number of clicks is ", me.numberOfClicks());
+  // just detect click for now
+  function processClickOnListItem(place) {
+
+    console.log("clicked on  ", place.name + " in the list.");
+
   }
 
-
   return me;
+
 })(ko, app.placeDataContext);
